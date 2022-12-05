dc8c6fb19ffcc4d48137443e8fe474712821690b
Andrew kibiwott <kibiandrew@gmail.com>
2022-12-05 18:05:30 +0300

    Add files via upload

---
+++ student grade generator/index.js
@@ -0,0 +1,26 @@
+const prompt=require("prompt-sync")();
+let mark=prompt("please enter marks");
+let marks=mark;
+function grade(){
+
+if(marks >= 80 && marks <= 100){
+console.log("A");
+}
+else if(marks >= 60 && marks <= 79){
+console.log("B");
+}
+else if(marks >= 50 && marks <=59){
+console.log("C");
+} 
+else if(marks>=40 && marks <= 49){
+console.log("D");
+}
+else if(marks <= 39){
+console.log("E");
+
+}
+else {
+console.log("INVALID MARKS");
+}
+}
+grade() 
